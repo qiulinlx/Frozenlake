c704d52f8003f0cae220ed75c7e9e001f288dea2
Qiulin <110373610+qiulinlx@users.noreply.github.com>
2023-03-21 12:57:05 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -3,6 +3,7 @@ from jax import random
 from jax import jit
 import Visual
 
+#Initialise env
 key = random.PRNGKey(1)
 env= Gridworld.Frozenlake()
 env.reset(key)
@@ -11,6 +12,7 @@ state, timestep = jit(env.reset)(key) #Get the state and timestep
 action = env.action_spec().generate_value() #Generate a valid action given the state
 state, timestep = jit(env.step)(state, action) #Tells the agent to take the action
 
+#Render method
 grid=state.grid
 elfpos=state.elf_position
 Visual.render(state.grid, elfpos)
